cc5365c1ca0b532338ad2e787a38b1d05eb59ad1
Stacey Borrego <staceyborrego@gmail.com>
2018-10-12 21:45:34 -0700

    adding script

---
+++ tif_to_jpeg_converstion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eoux pipefail
+
+PRJT_DIR=/home/stacey/hd2/image_conversion_project/
+TIF_DIR=${PRJT_DIR}/images
+
+JPG_DIR=${PRJT_DIR}/images_jpg
+
+mkdir -p ${JPG_DIR}
+
+find . -name *.tif -type f -print0 | xargs -0 -I cp ${JPG_DIR}
