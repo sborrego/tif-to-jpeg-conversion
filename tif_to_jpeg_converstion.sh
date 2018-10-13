4823820d8d9a8197545f7c9ad8c4bc88546f2a99
Stacey Borrego <staceyborrego@gmail.com>
2018-10-12 21:57:24 -0700

    added {} to xargs

--- tif_to_jpeg_converstion.sh
+++ tif_to_jpeg_converstion.sh
@@ -9,4 +9,4 @@ JPG_DIR=${PRJT_DIR}/images_jpg
 
 mkdir -p ${JPG_DIR}
 
-find ${TIF_DIR} -name *.tif -type f -print0 | xargs -0 -I cp -r ${JPG_DIR}
+find ${TIF_DIR} -name *.tif -type f -print0 | xargs -0 -I {} cp -r ${JPG_DIR}
